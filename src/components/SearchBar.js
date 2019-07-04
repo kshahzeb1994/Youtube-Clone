58d75b7d745912d5cfaaa12d656fcc480f94c06b
kshahzeb1994 <kshahzeb1994@gmail.com>
2019-07-04 14:34:18 -0700

    Added event listeners on the search bar along with semantic ui styling.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  }
 
   render() {
-    return <input type="text" placeholder="Search" />;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search: </label>
+            <input value={this.state.term} type="text" onChange={this.onInputChange}/>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
