4377832191507c1767cbfe0cf836e1b5b74780cc
kshahzeb1994 <kshahzeb1994@gmail.com>
2019-07-04 21:07:39 -0700

    The app is now able to make get requests to the youtube api.

--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const key = 'AIzaSyDg6HcbrGeTYeFpBUlixGq4USHHqi3osaM';
+const Key = 'AIzaSyDg6HcbrGeTYeFpBUlixGq4USHHqi3osaM';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar'
+import youtube from '../api/Youtube'
 
 class App extends React.Component {
   onTermSubmit = (term) => {
-    console.log(term);
-  }
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+  };
 
   render() {
     return  (
