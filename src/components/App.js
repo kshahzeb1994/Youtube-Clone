7e481f9634c006a5a990781f141397113fb7940e
kshahzeb1994 <kshahzeb1994@gmail.com>
2019-07-05 20:21:18 -0700

    Added VideoList component. It gets the list(array) of all videos from the parent's(App) state.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import SearchBar from './SearchBar'
-import youtube from '../api/Youtube'
+import SearchBar from './SearchBar';
+import youtube from '../api/Youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] }
@@ -18,7 +19,7 @@ class App extends React.Component {
     return  (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        Found { this.state.videos.length } videos
+        <VideoList videos= {this.state.videos}/>
       </div>
     );
   }
---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoList = (props) => {
+
+    return (
+      <div>
+        Found { props.videos.length } videos
+      </div>
+    );
+}
+
+export default VideoList;
