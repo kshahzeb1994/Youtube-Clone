fd103e6bf9a22d852181649a99ce5046e2cb6bf0
kshahzeb1994 <kshahzeb1994@gmail.com>
2019-07-05 22:13:51 -0700

    The VideoList component now displays video thumbnail and title. Styling still required.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-  return <div> VideoItem </div>;
+const VideoItem = ({ video }) => {
+  console.log(video);
+  return (
+      <div>
+        <img src={video.snippet.thumbnails.medium.url} />
+        <label>{video.snippet.title}</label>
+      </div>
+    );
 };
 
 export default VideoItem;
