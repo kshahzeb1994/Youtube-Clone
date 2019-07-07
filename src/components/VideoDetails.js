d6c42d6ff0e207a591d4085e340488b8936d79d5
kshahzeb1994 <kshahzeb1994@gmail.com>
2019-07-06 19:06:59 -0700

    Added video player for the selected video.

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const VideoDetails = ( {video} ) => {
   if(video) {
+    const videoSrc = `https://youtube.com/embed/${video.id.videoId}`;
     return (
       <div>
+        <div className="ui embed">
+          <iframe src={videoSrc} />
+        </div>
         <div className="ui segment">
           <h4 className="ui header">{video.snippet.title}</h4>
           <p>{video.snippet.description}</p>
